905c47f847b1c4257c2575e58a05d69c6fdfc752
Sarang <sarangahlawat73@gmail.com>
2024-07-08 19:02:49 +0530

    editted

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 let name = document.getElementById("name");
-let email = document.getElementById("email")
-let phone = document.getElementById("phone")
-let pass = document.getElementById("pass")
-let Cpass = document.getElementById("Cpass")
+let email = document.getElementById("email");
+let phone = document.getElementById("phone");
+let pass = document.getElementById("pass");
+let Cpass = document.getElementById("Cpass");
+let submission = 1;
+
+function validateForm() {
+    if(name.value == ""){
+        document.getElementById("name_error").innerHTML = "Name field cannot be empty"
+        console.log("empty");
+    }
+    return true;
+}
