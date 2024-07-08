5e74fbd526dfca3411c0149a2ee15027b4d77fea
Sarang <sarangahlawat73@gmail.com>
2024-07-08 17:18:43 +0530

    some edits and responsive

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-function errorOccured(){
-    
-}
+let name = document.getElementById("name");
+let email = document.getElementById("email")
+let phone = document.getElementById("phone")
+let pass = document.getElementById("pass")
+let Cpass = document.getElementById("Cpass")
